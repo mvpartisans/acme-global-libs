138337b339838c870ccbb40b246517853527d0eb
imranansari <imran.iansari@gmail.com>
2016-05-10 22:43:29 -0700

    init

--- refimpl/DpsLib.groovy
+++ refimpl/DpsLib.groovy
@@ -3,7 +3,7 @@
  */
 
 
-def invoke(def selector, boolean broadcast) {
+def init(def selector, boolean broadcast) {
 
     echo " Selector : ${selector} "
 
@@ -13,147 +13,39 @@ def invoke(def selector, boolean broadcast) {
     stage 'Copy Scripts'
     //checkpoint 'Copy Scripts'
 
-    runStep nodeLabels, envVars, broadcast, {
-        println "broadcast: ${broadcast}"
+    //runStep nodeLabels, envVars, broadcast, {
+    println "broadcast: ${broadcast}"
 
-        if (broadcast) {
-            println nodeLabels.size()
+    if (broadcast) {
+        println nodeLabels.size()
 
-            for (int i=0; i< nodeLabels.size(); i++){
+        for (int i = 0; i < nodeLabels.size(); i++) {
 
-                println "name of node: ${nodeLabels[i]}"
-                //println envVars["repo"]
+            println "name of node: ${nodeLabels[i]}"
+            //println envVars["repo"]
 
-                node(nodeLabels[i]) {
+            node(nodeLabels[i]) {
 /*                    bat "echo executing on node: ${nodeLabels[i]}"
                     bat "xcopy /s /v /z /y /i \"$MSI_STAGING_DIR\\*.*\" . "*/
-                }
             }
-
-        } else {
-            ///node(nodeLabels[0]) {
-            //   bat "echo executing on node: ${nodeLabels[0]}"
-            //   bat "xcopy /s /v /z /y /i \"$MSI_STAGING_DIR\\*.*\" . "
-            //}
         }
+
+    } else {
+        ///node(nodeLabels[0]) {
+        //   bat "echo executing on node: ${nodeLabels[0]}"
+        //   bat "xcopy /s /v /z /y /i \"$MSI_STAGING_DIR\\*.*\" . "
+        //}
     }
+    //}
 
-    stage 'Uninstall MSI'
-    runStep nodeLabels, envVars, broadcast, {
-        println "broadcast: ${broadcast}"
+}
 
-        if (broadcast) {
-            println nodeLabels.size()
 
-            for (int i=0; i< nodeLabels.size(); i++){
-
-                println "name of node: ${nodeLabels[i]}"
-                //println envVars["repo"]
-
-                node(nodeLabels[i]) {
-                    //bat "echo executing on node: ${nodeLabels[i]}"
-                    //bat "$PERL $bf_scripts\\uninstall_pp_msi.pl "
-                }
-            }
-
-        } else {
-            node(nodeLabels[0]) {
-                //bat "echo executing on node: ${nodeLabels[0]}"
-                //bat "$PERL $bf_scripts\\uninstall_pp_msi.pl"
-            }
-        }
-    }
-
-    stage 'Extract'
-    runStep nodeLabels, envVars, broadcast, {
-        println "broadcast: ${broadcast}"
-
-        if (broadcast) {
-            println nodeLabels.size()
-
-            for (int i=0; i< nodeLabels.size(); i++){
-
-                println "name of node: ${nodeLabels[i]}"
-                //println envVars["repo"]
-
-                node(nodeLabels[i]) {
-                    /**
-                     bat "echo executing on node: ${nodeLabels[i]}"
-                     bat "echo $shell=new-object -com shell.application  > $PS_UNZIP_SCRIPT"
-                     bat "echo $dest=$shell.namespace("${BF_ROOT}") >> $PS_UNZIP_SCRIPT"
-                     bat "echo $zips = get-childitem ${BF_ROOT}\\*.zip >> $PS_UNZIP_SCRIPT"
-                     bat "echo foreach ($zip in $zips) >> $PS_UNZIP_SCRIPT"
-                     bat "echo { >> $PS_UNZIP_SCRIPT"
-                     bat "echo Write-Host $zip.fullname >> $PS_UNZIP_SCRIPT"
-                     bat "echo $zipObj = $shell.namespace($zip.fullname) >> $PS_UNZIP_SCRIPT"
-                     bat "echo $dest.Copyhere($zipObj.items(),16) >> $PS_UNZIP_SCRIPT"
-                     bat "echo } >> $PS_UNZIP_SCRIPT"
-                     bat "$PS_EXE $BF_ROOT\\$PS_UNZIP_SCRIPT"
-                     **/
-/*                    bat "unzip -o -v *.zip"
-                    bat "dir /S /B"*/
-                }
-            }
-
-        } else {
-            node(nodeLabels[0]) {
-
-            }
-        }
-    }
-
-    stage 'Execute Install'
-    runStep nodeLabels, envVars, broadcast, {
-        println "broadcast: ${broadcast}"
-
-        if (broadcast) {
-            println nodeLabels.size()
-
-            for (int i=0; i< nodeLabels.size(); i++){
-
-                println "name of node: ${nodeLabels[i]}"
-                //println envVars["repo"]
-
-                node(nodeLabels[i]) {
-/*                    bat "echo executing on node: ${nodeLabels[i]}"
-                    bat "$PS_EXE $PS_DEPLOY_SCRIPT"*/
-                }
-            }
-
-        } else {
-            node(nodeLabels[0]) {
-/*                bat "echo executing on node: ${nodeLabels[0]}"
-                bat "$PS_EXE $PS_DEPLOY_SCRIPT"*/
-            }
-        }
-    }
-
-    stage 'Cleanup'
-    runStep nodeLabels, envVars, broadcast, {
-        println "broadcast: ${broadcast}"
-
-        if (broadcast) {
-            println nodeLabels.size()
-
-            for (int i=0; i< nodeLabels.size(); i++){
-
-                println "name of node: ${nodeLabels[i]}"
-                //println envVars["repo"]
-
-                node(nodeLabels[i]) {
-/*                    bat "echo executing on node: ${nodeLabels[i]}"
-                    bat "rm -rf $env.WORKSPACE*//*"
-                    bat "exit 0"*/
-                }
-            }
-
-        } else {
-            node(nodeLabels[0]) {
-/*                bat "echo executing on node: ${nodeLabels[0]}"
-                bat "rm -rf $env.WORKSPACE*//*"
-                bat "exit 0"*/
-            }
-        }
+def buildSteps(cl) {
+    def arr = cl.call();
+    arr.each {
+        println it.key
+        it.value.call();
     }
 }
 
