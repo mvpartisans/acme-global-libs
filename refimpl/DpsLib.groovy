887efdcbfd2e038b9cdc823553c6986ec0508254
imranansari <imran.iansari@gmail.com>
2016-05-10 22:55:11 -0700

    init

--- refimpl/DpsLib.groovy
+++ refimpl/DpsLib.groovy
@@ -44,6 +44,7 @@ def init(def selector, boolean broadcast) {
 def buildSteps(cl) {
     println "*******************************************************************************************************************"
     def arr = cl.call();
+    println arr.size()
     node() {
         arr.each {
             println it.key
