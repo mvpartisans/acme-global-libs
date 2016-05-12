59d5b5d0aa4bec9dbf2f59725ecd3aa46b4f113b
mvpartisans <imran.iansari@yahoo.com>
2016-05-11 17:55:59 -0700

    Create executeParallelOnAllNodes.groovy

---
+++ refimpl/executeParallelOnAllNodes.groovy
@@ -0,0 +1,23 @@
+node {
+
+def branches = [:]
+def names = nodeNames()
+for (int i=0; i<names.size(); ++i) {
+  def nodeName = names[i];
+  // Into each branch we put the pipeline code we want to execute
+  branches["node_" + nodeName] = {
+    node(nodeName) {
+      echo "Triggering on " + nodeName
+      sh 'echo hello Node'
+    }
+  }
+}
+
+parallel branches
+
+}
+
+@NonCPS
+def nodeNames() {
+  return jenkins.model.Jenkins.instance.nodes.collect { it.name }
+}
