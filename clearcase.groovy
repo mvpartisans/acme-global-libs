04d3abed1b9ddac9cc6e8aec414af6231c0e0cb9
mvpartisans <imran.iansari@yahoo.com>
2016-06-08 10:43:11 -0400

    Update clearcase.groovy

--- clearcase.groovy
+++ clearcase.groovy
@@ -7,7 +7,7 @@ def clearCaseMakeView(def primaryGroup, def streamName){
     env.CLEARCASE_PRIMARY_GROUP = primaryGroup
     vobName = "${primaryGroup}[3:]"
     println "vobname: ${vobName}"
-    //sh 'cleartool mkview -tag devops_{vobName} -tmode unix -stream  ${streamname}@/vobs/${vobName} -stgloc -auto'
+    //'cleartool mkview -tag devops_{vobName} -tmode unix -stream  ${streamname}@/vobs/${vobName} -stgloc -auto'
     def command =  "cleartool mkview -tag devops_${vobName} -tmode unix -stream  ${streamName}@/vobs/${vobName} -stgloc -auto"
     println command
     sh "${command}"
@@ -18,9 +18,10 @@ def clearCaseMakeView(def primaryGroup, def streamName){
  * @param PRIMARY_GROUP
  * @return
  */
-def cleanupClearCaseCheckout(PRIMARY_GROUP){
-    def vobName = "{${PRIMARY_GROUP}[3:]}"
-    sh 'cleartool rmview -tag devops_{vobName}'
+def clearCaseRemoveView(primaryGroup){
+    def vobName = "${primaryGroup}[3:]"
+    def command = "cleartool rmview -tag devops_${vobName}""
+    sh "${command}"
 }
 
 return this;
