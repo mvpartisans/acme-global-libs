1747d3a8aa11c639e6a902bbb2ee22b242fa2e44
mvpartisans <imran.iansari@yahoo.com>
2016-06-08 16:09:34 -0400

    Update ClearCaseJob.groovy

--- ClearCaseJob.groovy
+++ ClearCaseJob.groovy
@@ -5,8 +5,36 @@ def call(def cl) {
         println "***************************************"
         println e.getMessage();
     } finally {
-        utils.clearCaseRemoveView("FunApp");        
+        clearCaseRemoveView("FunApp");        
     }   
 }
+
+/**
+ * Make ClearCase View 
+ * @param PRIMARY_GROUP
+ * @return
+ */
+def clearCaseMakeView(def primaryGroup, def streamName){
+    env.CLEARCASE_PRIMARY_GROUP = primaryGroup
+    vobName = "${primaryGroup}[3:]"
+    println "vobname: ${vobName}"
+    //'cleartool mkview -tag devops_{vobName} -tmode unix -stream  ${streamname}@/vobs/${vobName} -stgloc -auto'
+    def command =  "cleartool mkview -tag devops_${vobName} -tmode unix -stream  ${streamName}@/vobs/${vobName} -stgloc -auto"
+    println command
+    //sh "${command}"
+}
+
+/**
+ * Cleanup View
+ * @param PRIMARY_GROUP
+ * @return
+ */
+def clearCaseRemoveView(def primaryGroup){
+    def vobName = "${primaryGroup}[3:]"
+    def command = "cleartool rmview -tag devops_${vobName}"
+    //sh "${command}"
+    println "cleanup"
+}
+
 
 return this;
