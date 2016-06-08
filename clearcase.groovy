dfa1905b80eae1c6643ed93f9c7dbdc7cc9848b0
mvpartisans <imran.iansari@yahoo.com>
2016-06-08 10:17:46 -0400

    Update clearcase.groovy

--- clearcase.groovy
+++ clearcase.groovy
@@ -1,8 +1,11 @@
-def checkoutScm(PRIMARY_GROUP){
+def checkoutFromClearCase(PRIMARY_GROUP){
+  env.CLEARCASE_PRIMARY_GROUP={PRIMARY_GROUP}
+  vobName = "{${PRIMARY_GROUP}[3:]}"
+  cleartool mkview -tag devops_{vobName} -tmode unix -stream  {streamname}@/vobs/{vobName} -stgloc -auto
+  cleartool setview devops_{vobName}
+}
 
-env.CLEARCASE_PRIMARY_GROUP={PRIMARY_GROUP}
-vobName = "{${PRIMARY_GROUP}[3:]}"
-echo cleartool mkview -tag devops_{vobName} -tmode unix -stream  {streamname}@/vobs/{vobName} -stgloc -auto
-echo cleartool setview devops_{vobName}
-echo cleartool rmview -tag devops_{vobName}
+def cleanupClearCaseCheckout(PRIMARY_GROUP){
+    vobName = "{${PRIMARY_GROUP}[3:]}"  
+    cleartool rmview -tag devops_{vobName}
 }
