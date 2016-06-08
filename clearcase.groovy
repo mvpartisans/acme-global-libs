21eb0b7bb9d2aa3fe808ba092749c870d391ebe0
mvpartisans <imran.iansari@yahoo.com>
2016-06-08 10:37:56 -0400

    Update clearcase.groovy

--- clearcase.groovy
+++ clearcase.groovy
@@ -8,8 +8,10 @@ def clearCaseMakeView(def primaryGroup, def streamName){
     vobName = "${primaryGroup}[3:]"
     println "vobname: ${vobName}"
     //sh 'cleartool mkview -tag devops_{vobName} -tmode unix -stream  ${streamname}@/vobs/${vobName} -stgloc -auto'
-    println "cleartool mkview -tag devops_${vobName} -tmode unix -stream  ${streamName}@/vobs/${vobName} -stgloc -auto"
-    //sh 'cleartool setview devops_{vobName}'
+    def command =  "cleartool mkview -tag devops_${vobName} -tmode unix -stream  ${streamName}@/vobs/${vobName} -stgloc -auto"
+    sh '''
+        command
+    '''
 }
 
 /**
