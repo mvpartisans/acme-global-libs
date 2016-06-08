459ff62f41985f6906af8495bc7a4e9b2a09e852
mvpartisans <imran.iansari@yahoo.com>
2016-06-08 10:21:32 -0400

    Update clearcase.groovy

--- clearcase.groovy
+++ clearcase.groovy
@@ -1,11 +1,21 @@
+/**
+ * Checkout from ClearCase
+ * @param PRIMARY_GROUP
+ * @return
+ */
 def checkoutFromClearCase(PRIMARY_GROUP){
-  env.CLEARCASE_PRIMARY_GROUP={PRIMARY_GROUP}
-  vobName = "{${PRIMARY_GROUP}[3:]}"
-  cleartool mkview -tag devops_{vobName} -tmode unix -stream  {streamname}@/vobs/{vobName} -stgloc -auto
-  cleartool setview devops_{vobName}
+    env.CLEARCASE_PRIMARY_GROUP={PRIMARY_GROUP}
+    vobName = "{${PRIMARY_GROUP}[3:]}"
+    sh 'cleartool mkview -tag devops_{vobName} -tmode unix -stream  {streamname}@/vobs/{vobName} -stgloc -auto'
+    sh 'cleartool setview devops_{vobName}'
 }
 
+/**
+ * Cleanup View
+ * @param PRIMARY_GROUP
+ * @return
+ */
 def cleanupClearCaseCheckout(PRIMARY_GROUP){
-    vobName = "{${PRIMARY_GROUP}[3:]}"  
-    cleartool rmview -tag devops_{vobName}
+    def vobName = "{${PRIMARY_GROUP}[3:]}"
+    sh 'cleartool rmview -tag devops_{vobName}'
 }
